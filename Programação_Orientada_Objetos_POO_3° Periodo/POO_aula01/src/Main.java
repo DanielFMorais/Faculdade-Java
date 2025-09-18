e42dab4bcd6226a25c293e47eadf05334e13293c
Daniel Fernandes de Morais <daniel.d.fm0512@gmail.com>
2025-09-17 21:22:23 -0300

    Primeira aula POO

---
+++ Programação_Orientada_Objetos_POO_3° Periodo/POO_aula01/src/Main.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int numeroConta = Integer.parseInt(JOptionPane.showInputDialog("Informe a conta:"));
+        String nome = JOptionPane.showInputDialog("Informe o Nome:");
+        double saldo = Double.parseDouble(JOptionPane.showInputDialog("Informe o saldo Incial"));
+
+        ContaBancaria conta = new ContaBancaria(numeroConta,nome,saldo);
+        conta.mostrarSaldo();
+        double valorAdicao = Double.parseDouble(JOptionPane.showInputDialog("Me informe quanto deseja adicionar"));
+
+        conta.realizarDeposito(valorAdicao);
+        conta.mostrarSaldo();
+
+        double valorSaque = Double.parseDouble(JOptionPane.showInputDialog("Por favor, me informe quanto que deseja sacar: "));
+        conta.realizarSaque(valorSaque);
+        conta.mostrarSaldo();
+
+
+    }
+}
