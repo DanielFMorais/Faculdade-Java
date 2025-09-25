e4c570555598491806fcb6e54006176f92beb1c6
Daniel Fernandes de Morais <daniel.d.fm0512@gmail.com>
2025-09-24 22:25:52 -0300

    Atualização atividade 18 POO

--- 3° Período/POO_Programacao_orientada_Objtetos/Lista de exercicios_AV1/Ativ18_lista_POO_AV1/src/Circulo.java
+++ 3° Período/POO_Programacao_orientada_Objtetos/Lista de exercicios_AV1/Ativ18_lista_POO_AV1/src/Circulo.java
@@ -1,26 +1,17 @@
 public class Circulo {
 
-    private double raio;
-    
-    public Circulo (double raio) {
+    double raio;
 
+    public Circulo (double raio){
         this.raio = raio;
-        
-    }
-    
-    public void calcularArea () {
-
-        double area = (Math.PI * Math.pow(raio, 2) );
-
-       System.out.printf("A área do circulo é de: %.2f", area);
     }
 
-    public void calcularPerimetro () {
+    public double areaCirculo (){
 
-        double perimetro = (2 * Math.PI * raio);
-
-        System.out.println("O Perímetro do Circulo é de: " + perimetro);
-
+        return (Math.PI*(raio * raio));
     }
 
+    public double perimetroCirculo (){
+        return  (2*Math.PI*raio);
+    }
 }
--- 3° Período/POO_Programacao_orientada_Objtetos/Lista de exercicios_AV1/Ativ18_lista_POO_AV1/src/Retangulo.java
+++ 3° Período/POO_Programacao_orientada_Objtetos/Lista de exercicios_AV1/Ativ18_lista_POO_AV1/src/Retangulo.java
@@ -1,27 +1,18 @@
 public class Retangulo {
-    private double base, altura;
+    double base;
+    double altura;
 
-    public Retangulo (double base, double altura) {
-
-        this.base = base;
+    public Retangulo (double base, double altura){
         this.altura = altura;
-
-    }
-    
-    public void areaRetangulo () {
-
-        double area = (base * altura);
-
-        System.out.println("A área do retangulo é de: " + area);
-
+        this.base = base;
     }
 
-    public void perimetroRetangulo () {
+    public double areaRetangulo (){
+        return (base * altura);
+    }
 
-        double perimetro = 2*(base + altura);
-
-        System.out.println("O perímetro do retangulo é de: " + perimetro);
-
+    public double perimetroRetangulo (){
+        return ((base + altura) * 2);
     }
 
 }
