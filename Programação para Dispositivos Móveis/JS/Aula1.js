036c1aa81e60dc2f5b239363a9b5750b3ee609dc
Daniel Fernandes de Morais <danielfm.office@gmail.com>
2026-08-10 22:25:32 -0300

    Add files via upload

---
+++ Programação para Dispositivos Móveis/JS/Aula1.js
@@ -0,0 +1,23 @@
+var idade = 12
+
+console.log(idade)
+
+const numero = 11 
+
+console.log(numero)
+
+let diaSemana = 'Segunda-Feira'
+
+console.log(diaSemana)
+
+console.log("---------------------------------------")
+
+const frutas = ["maça", "banana",  "uva"]
+
+console.log(frutas.length) // Conta quantos itens tem 
+
+console.log(frutas[0]) //Pega um item pelo índice
+console.log(frutas.push("morango")) // adiciona no final
+console.log("Tamanho do Array: " + frutas.length)
+console.log(frutas.pop()) //Remove o último
+console.log("Tamanho do Array: " + frutas.length)
